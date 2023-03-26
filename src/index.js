fdbcc00710959bbc5d5613494dda354acd94c999
KuhaiAnna <kuhai.anna@gmail.com>
2023-03-26 06:50:32 +0300

    Update index.js
    1. Remove the comments in the index.js file
    2. Add styles for loadMoreBtn
    3. Add new notification

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import './css/index.css';
-// Додатковий імпорт стилів для SimpleLightbox
-import 'simplelightbox/dist/simple-lightbox.min.css';
-import SimpleLightbox from 'simplelightbox';
 import { ImagePixabayService } from './js/requests/images';
 import { renderImageCard, removeMarkup } from './js/services/markup-image';
-import { onSearchError, onSearchSucces } from './js/notify/notification'; //onFetchError,
+import {
+  searchError,
+  searchSucces,
+  searchResulEnd,
+} from './js/notify/notification'; //fetchError,
 
 // Новий екземпляр класу
 const imagePixabayService = new ImagePixabayService();
@@ -16,6 +17,8 @@ export const refs = {
   loadMoreBtn: document.querySelector('.load-more'),
 };
 
+refs.loadMoreBtn.style.display = 'none'; //приховуємо кнопку за замовчуванням
+
 // Слухачі подій
 refs.formEl.addEventListener('submit', onSearch);
 refs.loadMoreBtn.addEventListener('click', onLoadMore);
@@ -24,43 +27,11 @@ refs.loadMoreBtn.addEventListener('click', onLoadMore);
 async function onSearch(e) {
   e.preventDefault();
 
-  removeMarkup();
+  removeMarkup(); // видаляємо розмітку при повторному сабміті
+  refs.loadMoreBtn.style.display = 'none'; // ховаємо кнопку при повторному сабміті
 
   imagePixabayService.query = e.target.elements.searchQuery.value.trim();
   imagePixabayService.resetPage();
-
-  // if (imagePixabayService.query !== '') {
-  //   getImages()
-  //     .then(data => {
-  //       if (data.hits.length === 0) {
-  //         throw new Error();
-  //         // onSearchError();
-  //       } else {
-  //         onSearchSucces(data);
-  //         renderImageCard(data.hits);
-  //       }
-  //     })
-  //     .catch(onSearchError);
-  // } else {
-  //   removeMarkup();
-  // }
-
-  // if (imagePixabayService.query !== '') {
-  //   imagePixabayService
-  //     .fetchImages()
-  //     .then(data => {
-  //       if (data.hits.length === 0) {
-  //         throw new Error();
-  //         // onSearchError();
-  //       } else {
-  //         onSearchSucces(data);
-  //         renderImageCard(data.hits);
-  //       }
-  //     })
-  //     .catch(onSearchError);
-  // } else {
-  //   removeMarkup();
-  // }
 
   if (imagePixabayService.query !== '') {
     try {
@@ -69,12 +40,17 @@ async function onSearch(e) {
       if (data.hits.length === 0) {
         throw new Error();
         // onSearchError();
-      } else {
-        onSearchSucces(data);
+      } else if (data.hits.length < 40) {
+        searchSucces(data);
         renderImageCard(data.hits);
+      } else {
+        searchSucces(data);
+        renderImageCard(data.hits);
+
+        refs.loadMoreBtn.style.display = 'block'; //багато картинок, показуємо кнопку loadMoreBtn
       }
     } catch {
-      onSearchError;
+      searchError();
     }
   } else {
     removeMarkup();
@@ -85,156 +61,14 @@ async function onSearch(e) {
 async function onLoadMore() {
   try {
     const data = await imagePixabayService.fetchImages();
+
+    if (data.hits.length < 40) {
+      searchResulEnd();
+      refs.loadMoreBtn.style.display = 'none';
+    }
+
     return renderImageCard(data.hits);
   } catch {
-    onSearchError;
+    searchError();
   }
 }
-
-// // Динамічне видалення розмітки
-// function removeMarkup() {
-//   refs.galleryEl.innerHTML = '';
-// }
-
-// -------
-// Стварення та рендеринг розмітки
-// function renderImageCard(array) {
-//   const imageCardMarkup = array
-//     .map(
-//       ({
-//         webformatURL,
-//         tags,
-//         likes,
-//         views,
-//         comments,
-//         downloads,
-//       }) => `<div class="photo-card">
-//   <img src="${webformatURL}" alt="${tags}" loading="lazy" />
-//   <div class="info">
-//     <p class="info-item">
-//       <b>Likes</b>${likes}
-//     </p>
-//     <p class="info-item">
-//       <b>Views</b>${views}
-//     </p>
-//     <p class="info-item">
-//       <b>Comments</b>${comments}
-//     </p>
-//     <p class="info-item">
-//       <b>Downloads</b>${downloads}
-//     </p>
-//   </div>
-// </div>`
-//     )
-//     .join('');
-
-//   // refs.galleryEl.insertAdjacentHTML('beforeend', imageCardMarkup);
-// }
-
-// // Пошук картинки
-// function onSearch(e) {
-//   e.preventDefault();
-
-//   removeMarkup();
-
-//   imageApiService.query = e.target.elements.searchQuery.value.trim();
-//   imageApiService.resetPage();
-
-//   if (imageApiService.query !== '') {
-//     imageApiService
-//       .fetchImages()
-//       .then(data => {
-//         if (data.hits.length === 0) {
-//           throw new Error();
-//           // onSearchError();
-//         } else {
-//           onSearchSucces(data);
-//           renderImageCard(data.hits);
-//         }
-//       })
-//       .catch(onSearchError);
-//   } else {
-//     removeMarkup();
-//   }
-// }
-
-// // Завантаженн більшої кількості картинок
-// function onLoadMore() {
-//   imageApiService
-//     .fetchImages()
-//     .then(data => renderImageCard(data.hits))
-//     .catch(onFetchError);
-// }
-
-// // -------
-// // Стварення та рендеринг розмітки
-// function renderImageCard(array) {
-//   const imageCardMarkup = array
-//     .map(
-//       ({
-//         webformatURL,
-//         tags,
-//         likes,
-//         views,
-//         comments,
-//         downloads,
-//       }) => `<div class="photo-card">
-//   <img src="${webformatURL}" alt="${tags}" loading="lazy" />
-//   <div class="info">
-//     <p class="info-item">
-//       <b>Likes</b>${likes}
-//     </p>
-//     <p class="info-item">
-//       <b>Views</b>${views}
-//     </p>
-//     <p class="info-item">
-//       <b>Comments</b>${comments}
-//     </p>
-//     <p class="info-item">
-//       <b>Downloads</b>${downloads}
-//     </p>
-//   </div>
-// </div>`
-//     )
-//     .join('');
-
-//   refs.galleryEl.insertAdjacentHTML('beforeend', imageCardMarkup);
-// }
-
-// // Динамічне видалення розмітки
-// function removeMarkup() {
-//   refs.galleryEl.innerHTML = '';
-// }
-
-// import { Notify } from 'notiflix/build/notiflix-notify-aio';
-
-// Налаштування сповіщень
-// export const notiflixOptions = {
-//   width: '330px',
-//   fontSize: '16px',
-//   position: 'right-top',
-//   timeout: 2500,
-// };
-
-// // Сповіщення про загальну помилку
-// function onFetchError() {
-//   // Notify.failure(`Error! Status ${status}`, notiflixOptions);
-
-//   Notify.failure(
-//     'Oooops! Something went wrong. Please try again later.',
-//     notiflixOptions
-//   );
-// }
-
-// // Сповіщення про помилку пошуку
-// function onSearchError() {
-//   Notify.failure(
-//     'Sorry, there are no images matching your search query. Please try again.',
-//     notiflixOptions
-//   );
-// }
-
-// // Сповіщення про результат пошуку
-// function onSearchSucces({ totalHits }) {
-//   Notify.success(`Hooray! We found ${totalHits} images.`, notiflixOptions);
-// }
