e13c9f2410201ecd63496164c8d2ae52a766ff58
KuhaiAnna <kuhai.anna@gmail.com>
2023-03-26 18:22:32 +0300

    Change markup

--- src/js/services/markup-image.js
+++ src/js/services/markup-image.js
@@ -23,23 +23,24 @@ export function renderImageCard(array) {
         views,
         comments,
         downloads,
-      }) => `<div class="photo-card">
-  <a class="image-link" href="${largeImageURL}"><img class="photo" src="${webformatURL}" alt="${tags}" loading="lazy" /></a>
-  <div class="info">
-    <p class="info-item">
-      <b>Likes</b> ${likes}
-    </p>
-    <p class="info-item">
-      <b>Views</b> ${views}
-    </p>
-    <p class="info-item">
-      <b>Comments</b> ${comments}
-    </p>
-    <p class="info-item">
-      <b>Downloads</b> ${downloads}
-    </p>
-  </div>
-</div>`
+      }) => `<a class="image-link link" href="${largeImageURL}">
+              <div class="photo-card">
+                <img class="photo" src="${webformatURL}" alt="${tags}" loading="lazy" />
+                <div class="info">
+                  <p class="info-item">
+                    <b>Likes</b> ${likes}
+                  </p>
+                  <p class="info-item">
+                    <b>Views</b> ${views}
+                  </p>
+                  <p class="info-item">
+                    <b>Comments</b> ${comments}
+                  </p>
+                  <p class="info-item">
+                    <b>Downloads</b> ${downloads}
+                  </p>
+                </div>
+              </div></a>`
     )
     .join('');
 
